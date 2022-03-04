f640093371d5bf8c0067772d5cc3abfebf940667
Alviss Raghnall <isiomastewart@gmail.com>
2022-03-04 03:46:50 -0800

    added spring boot

---
+++ src/main/java/com/alviss/web3/firsteth/FirstEthApplication.java
@@ -0,0 +1,13 @@
+package com.alviss.web3.firsteth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import com.alviss.web3.firsteth.;
+
+@SpringBootApplication
+public class FirstEthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FirstEthApplication.class, args);
+	}
+}
